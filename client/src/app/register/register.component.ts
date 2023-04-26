e146ed70f60a61fd3319d23d76f1b0db6716e133
ChrisHaslam93 <haslam.consultancy@gmail.com>
2023-04-26 12:13:40 +0100

    Added more frontend code

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  @Output() cancelRegister = new EventEmitter();
+
+  constructor(private accountService: AccountService) {}
+  
+  model: any = {};
+
+  register() {
+    this.accountService.register(this.model).subscribe({
+      next: () => { 
+        this.cancel();
+      },
+      error: err => console.log(err)      
+    })
+  }
+
+  cancel(){
+    this.cancelRegister.emit(false);
+  }
+}
